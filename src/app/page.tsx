f6cf1cb452178c4d247e398e92021ef5222c70de
remilekun-hub <atandaremilekun@gmail.com>
2025-02-18 15:24:19 +0100

    added total and current pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,6 +152,8 @@ export default function Home() {
 						>
 							Prev
 						</button>
+
+						{currentPage}  of {totalPages}
 						
 						<button
 							onClick={() => setCurrentPage((cur) => cur + 1)}
